d19a675c2226fb47e8633d9657572cc7dc2c58a5
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2017-03-31 15:10:55 -0700

    fork process on each call

--- server.c
+++ server.c
@@ -3,13 +3,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+void dostuff(int); /* function prototype */
 void error(char* msg){
 	perror(msg);
 	exit(1);
 }
 
 int main(int argc, char*argv[]){
-	int sockfd, newsockfd, portno, clilen;
+	int sockfd, newsockfd, portno, clilen, pid;
 	char buffer[256];
 	struct sockaddr_in serv_addr, cli_addr;
 	int n;
@@ -36,20 +37,47 @@ int main(int argc, char*argv[]){
 	}
 
 	if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) error("ERROR on binding\n");
-	listen(sockfd, 5);
+	listen(sockfd, 2);
+
+	clilen = sizeof(cli_addr);
 
 	while(1){
-	clilen = sizeof(cli_addr);
-	newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
-	if(newsockfd < 0)error("ERROR on accept\n");
-
-	bzero(buffer, 256);
-	n = read(newsockfd, buffer, 255);
-	if(n < 0)error("ERROR reading from socket\n");
-	printf("Here is message: %s\n", buffer);
-	n = write(newsockfd, "I got your message\n", 19);
-	if(n<0)error("ERROR writing to socket\n");
-	close(newsockfd);
+		newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+		if(newsockfd < 0)error("ERROR on accept\n");
+		pid = fork();
+		if (pid < 0)
+			error("ERROR on fork");
+		if (pid == 0)  {
+			close(sockfd);
+			dostuff(newsockfd);
+			exit(0);
+		}
+		else close(newsockfd);
+//	bzero(buffer, 256);
+//	n = read(newsockfd, buffer, 255);
+//	if(n < 0)error("ERROR reading from socket\n");
+//	printf("Here is message: %s\n", buffer);
+//	n = write(newsockfd, "I got your message\n", 19);
+//	if(n<0)error("ERROR writing to socket\n");
+	//close(newsockfd);
 	}
 	return 0;
+}
+
+/******** DOSTUFF() *********************
+ There is a separate instance of this function
+ for each connection.  It handles all communication
+ once a connnection has been established.
+ *****************************************/
+void dostuff (int sock)
+{
+   int n;
+   char buffer[256];
+
+   bzero(buffer,256);
+   n = read(sock,buffer,255);
+   if (n < 0) error("ERROR reading from socket");
+   printf("Here is the message: %s\n",buffer);
+   n = write(sock,"I got your message",18);
+   if (n < 0) error("ERROR writing to socket");
 }
